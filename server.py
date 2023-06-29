502c8ab4cc07528a89778f07f3df25d305bb9f4e
Amit Kumar <76451362+freak4coding@users.noreply.github.com>
2023-06-29 14:41:54 +0530

    Update server.py
    changed port

--- server.py
+++ server.py
@@ -104,6 +104,6 @@ def ActivityList():
     return jsonify(ActivityList)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    app.run(debug=True)
     # Pipelines()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
