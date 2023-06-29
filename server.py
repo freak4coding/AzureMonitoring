2c09f9a58c3e2341639a14cf20309e6d8637e036
Amit Kumar <76451362+freak4coding@users.noreply.github.com>
2023-06-29 15:43:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -16,10 +16,13 @@ completeDataList["lastTimeBefore"]=""
 completeDataList["RunId"]=""
 
 @app.route('/', methods=['Get'])
-def subscription():
-    subscriptionList = azureMonitoring.getListOfSubscription()
-    return jsonify(subscriptionList)
-
+# def subscription():
+#     subscriptionList = azureMonitoring.getListOfSubscription()
+#     return jsonify(subscriptionList)
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
 @app.route('/resourceGroup', methods=['Post','Get'])
 def resourceGroup():
