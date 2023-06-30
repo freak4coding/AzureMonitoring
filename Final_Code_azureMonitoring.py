d9850b885ad74d24ef9a3d11032381d864cb725b
Amit Kumar <76451362+freak4coding@users.noreply.github.com>
2023-06-30 09:42:44 +0530

    Update Final_Code_azureMonitoring.py

--- Final_Code_azureMonitoring.py
+++ Final_Code_azureMonitoring.py
@@ -1,21 +1,10 @@
-# importing all the necessary libraries
-# import datetime
-# from azure.identity import ClientSecretCredential
 from azure.identity import InteractiveBrowserCredential
-# from azure.identity import AzureCliCredential
 # from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.resource import SubscriptionClient
 from azure.mgmt.datafactory import DataFactoryManagementClient
 from azure.mgmt.datafactory.models import *
-# import server
-# import re
-# import sys
-# import threading
-# import csv
 
-# f = open("FinalOutput.csv","w")
-# sys.stdout = f
 credential = InteractiveBrowserCredential()
 
 class RunHistory:
@@ -66,27 +55,20 @@ class RunHistory:
         return(CompleteData)
 
     def get_activity_list(subscription_id, resource_group_name, data_factory_name, pipeline_run_id,LastUpdateAfter,LastUpdateBefore):
-        # ActivityResponse = [{"PipelineRunId":pipeline_run_id}]
         ActivityResponse = []
         if pipeline_run_id != "":    
             adf_client =  DataFactoryManagementClient(credential, subscription_id)
-                # pipeline_run = adf_client.pipeline_runs.get(resource_group_name, data_factory_name, pipeline_run_id)
             try:                      
                 filter_params = RunFilterParameters(last_updated_after = RunHistory.format_LTA_LTB(LastUpdateAfter),
                             last_updated_before = RunHistory.format_LTA_LTB(LastUpdateBefore))                            
                 query_response = adf_client.activity_runs.query_by_pipeline_run(resource_group_name,data_factory_name,pipeline_run_id,filter_params)    
                 for out in query_response.value:
                     if out.activity_type == "ExecutePipeline":
-                                        # print("{},{},{},{},{},{},{}".format(out.activity_type,out.activity_name,out.status,out.run_start,out.run_end,out.duration_in_ms))
-                                        # ActivityResponse.append({"ActivityName":out.input["pipeline"]["referenceName"],"Status":out.status,"Duration":RunHistory.timeConversion(out.duration_in_ms/1000),"ErrorMesssage":run.message})
                         ActivityResponse.append({"ActivityRunId":out.activity_run_id,"Type":"Execute Activity","ActivityName":out.input["pipeline"]["referenceName"],"Status":out.status,"RunStartDate":out.activity_run_start,"RunEndDate":out.activity_run_end,"Duration":out.duration_in_ms/1000,"ErrorMessage":RunHistory.printFailureMessage(out.error["message"])})
                     else:
-                        ActivityResponse.append({"ActivityRunId":out.activity_run_id,"Type":"Activity","ActivityName":out.activity_name,"Status":out.status,"RunStartDate":out.activity_run_start,"RunEndDate":out.activity_run_end,"Duration":out.duration_in_ms/1000,"ErrorMessage":RunHistory.printFailureMessage(out.error["message"])})
-                        #     # # print(ActivityResponse)
-                        #     # print(ActivityResponse)                      
+                        ActivityResponse.append({"ActivityRunId":out.activity_run_id,"Type":"Activity","ActivityName":out.activity_name,"Status":out.status,"RunStartDate":out.activity_run_start,"RunEndDate":out.activity_run_end,"Duration":out.duration_in_ms/1000,"ErrorMessage":RunHistory.printFailureMessage(out.error["message"])})                     
             except Exception as e:
-                print(e)
-                        # # run["ActivityResponse"] = ActivityResponse  
+                print(e) 
         else:
             print("")
         return ActivityResponse
@@ -99,7 +81,6 @@ class SubscriptionDetail:
         PipelineList = []
         for pipeline in pipelines:
             PipelineList.append(pipeline.name)
-        # RunHistory.get_master_pipeline_adf(writer,credential,subscriptionID,resourceGroupName,dataFactoryName,pipelineName)
         return PipelineList
 
     def getResourceGroup(subscriptionID):
@@ -107,32 +88,28 @@ class SubscriptionDetail:
         resources = resource_client.resource_groups.list()
         resourceGroupList = []
         for resource in resources:
-            # print("Resource Groupe Name : {}\t Type: {}".format(resource.name,resource.type))
             resourceGroupList.append(resource.name)
         return resourceGroupList 
     def getListOfADFs(subscriptionID, resourceGroupName):
-        # resources = resource_client.resource_groups.list()
         resource_client = ResourceManagementClient(credential, subscription_id=subscriptionID)
         services = resource_client.resources.list_by_resource_group(resourceGroupName)
         ADFList = []
         for service in services:
             if (service.type == "Microsoft.DataFactory/factories"):
-                # print("Data Factory Name : "+service.name+"\n")
                 ADFList.append(service.name) 
         return ADFList
                 
-def getListOfSubscription():
-    # credential = InteractiveBrowserCredential()
+def getListOfSubscription(credential):
     subscription_client = SubscriptionClient(credential)
     subscription_list = subscription_client.subscriptions.list()
     SubscriptionList={}
     for subscription in subscription_list:
-        # print("Subscription ID : {}\nSubscriptionName : {}\n".format(subscription.subscription_id, subscription.display_name))
         SubscriptionList[subscription.display_name]=subscription.subscription_id
     return SubscriptionList
 
  
 def main():
     getListOfSubscription()
+    
 if __name__ == "__main__":
     main()
